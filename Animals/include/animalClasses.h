7daa2e60008eb91f4f7fe257789d55eb34f5a010
leylaiskandarli <zcapisk@ucl.ac.uk>
2025-02-07 16:19:38 +0000

    Updated header file to declare the Animal base class and its derived classes (Dog, Cat, Budgie).

--- Animals/include/animalClasses.h
+++ Animals/include/animalClasses.h
@@ -1,1 +1,33 @@
 #pragma once
+#ifndef ANIMALCLASSES_H
+#define ANIMALCLASSES_H
+
+#include <iostream>
+
+// Base class
+class Animal {
+public: 
+    virtual void speak() const = 0; // Pure virtual function
+    virtual ~Animal() {} // Virtual destructor 
+
+};
+
+// Derived class: Dog 
+class Dog : public Animal {
+public:
+    void speak() const override;
+};
+
+// Derived class: Cat 
+class Cat : public Animal {
+public: 
+    void speak() const override;
+};
+
+// Derived class: Budgie 
+class Budgie : public Animal {
+public: 
+    void speak() const override;
+};
+
+#endif // ANIMALCLASSES_H
